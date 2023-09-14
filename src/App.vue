68d088f7ca320df2066461b28bc5c6b547ac83f8
Mary Williams <sbyardnss@gmail.com>
2023-09-14 13:25:43 -0500

    board component added

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import "vue-chessground/chessboard.css";
+import ChessBoardDisplay from "./components/ChessBoardDisplay.vue";
+</script>
+<template>
+  <div>
+    <h2>hellow wo</h2>
+    <ChessBoardDisplay />
+  </div>
+</template>
---
+++ src/components/ChessBoardDisplay.vue
@@ -0,0 +1,16 @@
+<template>
+  <Chessboard />
+</template>
+
+<script>
+import { Chessboard } from "vue-chessground";
+import "vue-chessground/chessboard.css";
+export default {
+  name: "ChessBoardDisplay",
+  components: {
+    Chessboard,
+  },
+};
+</script>
+
+<style></style>
